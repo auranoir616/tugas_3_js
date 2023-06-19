bd93c60bde143d2ce0382e8716da80b6c13f12ec
febri <fitrafilthmongers@gmail.com>
2023-06-19 21:05:13 +0700

    tugas 3 js
    mengumpulkan tugas 3 js

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+function ubahnilai(nilai){
+    console.log("nilai sebelum :",nilai);
+
+    nilai = nilai * 2
+    console.log("nilai sesudah :",nilai);
+return nilai;
+}
+
+var nilaiawal = 6
+var nilaiakhir = ubahnilai(nilaiawal)
+
